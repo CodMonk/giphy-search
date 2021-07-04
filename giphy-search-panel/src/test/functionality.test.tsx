b5c631e651ef1b8e7b0f7913e72eaaddd52355c0
Cod Monk <abhishek2142001@gmail.com>
2021-07-04 18:42:21 +0530

    test cases for seach button functionality

--- giphy-search-panel/src/test/functionality.test.tsx
+++ giphy-search-panel/src/test/functionality.test.tsx
@@ -9,6 +9,23 @@ describe("FUNCTIONALITY TESTS", () => {
   it("should not give error when user clicks on button without entering search string", () => {
     wrapper.find("#search-button").simulate("click");
   });
+  it("search button should remain disabled if text is empty",()=>{
+
+    const searchInput = wrapper.find('#search-input');
+    searchInput.simulate('change', { target: { value: '' } });
+    searchInput.update();
+    const searchButton = wrapper.find('#search-button');
+    expect(searchButton.prop('disabled')).toBe(true);
+  });
+
+  it("search button should enable if text is not empty",()=>{
+
+    const searchInput = wrapper.find('#search-input');
+    searchInput.simulate('change', { target: { value: 'cake' } });
+    searchInput.update();
+    const searchButton = wrapper.find('#search-button');
+    expect(searchButton.prop('disabled')).toBe(false);
+  })
 
 });
 // describe("FUNCTIONALITY TESTS", () => {
